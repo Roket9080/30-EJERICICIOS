8018bde452128a8d946270257aefc1c2c2197e87
Miguelito90 <migueherazo2004@gmail.com>
2026-03-14 22:14:08 -0500

    se crearon nuevos case

--- menu.js
+++ menu.js
@@ -12,16 +12,66 @@ function ejecutarEjercicio(){
     switch(ejercicio){
 
         case "1":
-            ejercicio1();
+            mostrarnumeros();
         break;
 
         case "2":
-            ejercicio2();
+            sumarNumeros();
         break;
 
         case "3":
-            ejercicio3();
+            mostrarPares();
         break;
+
+        case "4":
+            tablaMultiplicar();
+        break;
+
+        case "5":
+            calcularFactorial();
+        break;
+        
+        case "6":
+            procesarNumero();
+        break;
+        
+        case "7":
+            cargarSuma();
+        break;
+
+        case "8":
+            procesarLimite();
+        break;
+
+        case "9":
+            mostrarPotencia()
+        break;
+        
+        case "10":
+            procesarTerminos();
+        break;
+        
+        case "11":
+            procesarVector();
+        break;
+        
+        case "12":
+            mostrarMaximo();
+        break;
+        
+        case "13":
+            procesarBusqueda();
+        break;
+        
+        case "14":
+            reverseArray();
+        break;    
+
+        case "15":
+            countEvenNumbers();
+        break;    
+        
+        
 
         case "26":
             encontrarSubarrayMaximo();
