3f1ef91ae7eb935fd98c2b4c1f0307e285f072df
Miguelito90 <migueherazo2004@gmail.com>
2026-03-21 18:36:06 -0500

    se hicieron modificaciones en los ejercicios 15 y 16

--- integrantes/cristian/ejercicio15.js
+++ integrantes/cristian/ejercicio15.js
@@ -1,28 +1,11 @@
-export function countEvenNumbers() {
-    const input = document.getElementById("numbersInput").value;
-
-    if (!input) {
-        document.getElementById("resultado").innerHTML = "Ingresa números válidos.";
-        return;
-    }
-
-    // Convertir texto en array de números
-    const numbers = input
-        .split(",")
-        .map(n => Number(n.trim()))
-        .filter(n => !isNaN(n));
-
-    if (numbers.length === 0) {
-        document.getElementById("resultado").innerHTML = "No ingresaste números válidos.";
-        return;
-    }
-
+export function countEvenNumbers(numbers) {
     let contador = 0;
 
     for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] % 2 === 0) contador++;
+        if (numbers[i] % 2 === 0) {
+            contador++;
+        }
     }
 
-    document.getElementById("resultado").innerHTML =
-        `Cantidad de números pares: ${contador}`;
+    return contador;
 }
